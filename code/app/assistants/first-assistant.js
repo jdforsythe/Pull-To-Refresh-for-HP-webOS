5957ee4aed21a89c7e3bb4d85db764c7fc9b8edb
Jeremy Forsythe <jdfsoftware@gmail.com>
2011-01-18 13:04:47 -0500

    reset msg on auto-release, genericized methods for ease of plugging into apps

--- code/app/assistants/first-assistant.js
+++ code/app/assistants/first-assistant.js
@@ -7,16 +7,75 @@ function FirstAssistant() {
 
 FirstAssistant.prototype.setup = function() {
 
-   // just to show that it's working
+   // just to show that it's working - you would not use this in your real app
    this.refreshCount = 0;
+   
+   // DOM reference to scene content container
+   this.sceneContainer = this.controller.get("first-container");
+   
+   // setup pull to refresh
+   this.setupPullToRefresh();
+   
+
+};
+
+FirstAssistant.prototype.refreshContent = function() {
+	   // show the refresh block on the scene and show the refreshing msg instead of the time
+	   this.refreshBlock.className = "vis";
+		this.refreshTime.innerHTML = this.refreshingMsg;
+		
+		
+		// this is where you'd refresh your content (Ajax Requests, list items, etc) and in the FINAL CALLBACK
+		// you'd call the this.finishedRefresh() method (don't forget to bind correctly)
+		
+		
+		
+		// to simulate a real refresh for the example, we'll use a setTimeout to keep the refresh
+		// block on the scene for a couple seconds
+		
+		// wait to simulate a refreshing list
+		setTimeout( function() {
+		      var that = this; // binding
+		      
+            // set the refresh count (this is only so you can visibly see that it's refreshing)
+            // this would not be in your typical app
+		      this.refreshCount += 1;
+		      this.sceneContainer.innerHTML = "Refreshes: " + this.refreshCount;
+
+		      // we're done "refreshing" so reset the refresh block to invisible and set the time
+		      that.finishedRefresh();
+
+		   }.bind(this),
+		   3000 // wait 3 sec for effect
+		);
+};
+
+FirstAssistant.prototype.finishedRefresh = function() {
+
+		      // leave the refreshed message showing for .75 seconds after the time updates
+		      setTimeout( function(){this.refreshBlock.className = "invis";}.bind(this), 750);
+		      
+		      this.refreshText.innerHTML = this.pullDownMsg;
+	         this.refreshTime.innerHTML = Date().toString();
+	         this.refreshArrow.style["-webkit-transform"] = "rotate(0deg)";
+
+};
+
+FirstAssistant.prototype.setupPullToRefresh = function() {
+   // messages to show
+   this.pullDownMsg = "Pull Down to Refresh...";
+   this.releaseMsg = "Release to Refresh...";
+   this.refreshingMsg = '<div id="spinny"></div><span>Refreshing.. please wait..</span>';
 
    // DOM references
    this.scroller = this.controller.get("scroller");
    this.refreshBlock = this.controller.get("pullToRefresh");
    this.refreshText = this.controller.get("refreshText");
    this.refreshTime = this.controller.get("refreshTime");
-   this.sceneContainer = this.controller.get("first-container");
    this.refreshArrow = this.controller.get("refreshArrow");
+   
+   // start the refresh text as the pull down message
+   this.refreshText.innerHTML = this.pullDownMsg;
    
    // bindings
    this.scrollStarted = this.scrollStarted.bind(this);
@@ -40,7 +99,7 @@ FirstAssistant.prototype.scrolling = function(event) {
 	   // Make sure the user REALLY meant to refresh by making them hold it there for 50ms.
 		setTimeout(function() {
 			Mojo.Event.listen(this.scroller, "mouseup", this.releaseScroll);
-			this.refreshText.innerHTML = "Release to refresh...";
+			this.refreshText.innerHTML = this.releaseMsg;
 			this.refreshArrow.style["-webkit-transform"] = "rotate(180deg)";
 		}.bind(this), 50);
 		
@@ -53,37 +112,15 @@ FirstAssistant.prototype.releaseScroll = function(event) {
 	if(scrollY <= 20) {
 		//webOS did the thing that doesn't let you hold the list past the regular scroll limit :(
 		//Don't refresh because it's possible that it wasn't the user's intention to refresh...
+		// reset the message
+		this.refreshText.innerHTML = this.pullDownMsg;
 	}
 	
-	//else if(document.getElementById("pullTxt").innerHTML == "Release to refresh.") {
+   // otherwise it's a valid refresh
 	else {
-	   // show the refresh block on the scene
-	   this.refreshBlock.className = "vis";
+	
+	   this.refreshContent();
 	   
-		// just show that it's working (you'd probably want to put a spinner here instead)
-		this.refreshTime.innerHTML = '<div id="spinny"></div><span>Refreshing.. please wait..</span>';
-		
-		// instead of this setTimeout, you'd have the call to refresh your data and in the
-		// final callback after the scene is updated, you'd call the "reset the refresh block" code below
-		
-		// wait to simulate a refreshing list
-		setTimeout( function() {
-		      var that = this;
-            // set the refresh count (this is only so you can visibly see that it's refreshing)
-		      this.refreshCount += 1;
-		      this.sceneContainer.innerHTML = "Refreshes: " + this.refreshCount;
-
-		      // reset the refresh block to invisible, reset the message, and set the refresh time
-		      
-		      // leave the refreshed message showing for .75 seconds after the time updates
-		      setTimeout( function(){this.refreshBlock.className = "invis";}.bind(that), 750);
-		      
-		      this.refreshText.innerHTML = "Pull down to refresh...";
-	         this.refreshTime.innerHTML = Date().toString();
-	         this.refreshArrow.style["-webkit-transform"] = "rotate(0deg)";
-		   }.bind(this),
-		   2500 // wait 2.5 seconds for effect
-		);
 	}
 	
 	// whether it's a real refresh or not, remove the listeners
@@ -102,6 +139,4 @@ FirstAssistant.prototype.deactivate = function(event) {
 };
 
 FirstAssistant.prototype.cleanup = function(event) {
-	/* this function should do any cleanup needed before the scene is destroyed as 
-	   a result of being popped off the scene stack */
 };
